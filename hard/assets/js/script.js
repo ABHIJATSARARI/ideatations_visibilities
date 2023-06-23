03a69d2d876ccffb44bd7a285a86f7e16fae5a8f
ABHIJAT SARARI (Rapidhunter) <75532296+ABHIJATSARARI@users.noreply.github.com>
2023-06-23 15:05:18 +0530

    Add files via upload

---
+++ hard/assets/js/script.js
@@ -0,0 +1,6 @@
+document.getElementById('sendInvoice').onsubmit = function () {
+    return window.prompt('Form Submitted, enter ok');
+}
+function myFunc1() {
+    window.location.href = "index.html";
+}
